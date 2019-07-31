fa6d00b18bd073976e99e15a909930d2e9de0b49
Fleford Redoloza <fleford@gmail.com>
2019-07-30 21:13:55 -0600

    practice with FFT

--- WaveformAnalysis.py
+++ WaveformAnalysis.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
 
 # Import data
 data_array = np.loadtxt("CoolTerm Capture 2019-07-29 11-46-17.txt")
@@ -8,5 +10,21 @@ times_cumulative = np.zeros_like(data_array[:, 0])
 times_cumulative[0] = data_array[0, 0]
 for i in range(len(times_cumulative)-1):
     times_cumulative[i + 1] = data_array[i + 1, 0] + times_cumulative[i]
-print(data_array[:, 0])
-print(times_cumulative)
+
+# Replace interval times with cumulative times
+data_array = np.insert(data_array, 1, times_cumulative, axis=1)   # Insert time_cumulative in the middle column
+data_array = np.delete(data_array, 0, axis=1)   # Remove interval time column
+
+# Pick out a sample of the data array
+window_start = 0
+window_size = 100
+data_array_sample = data_array[window_start:window_start+window_size, :]
+y, x = signal.resample(data_array_sample[:, 1], 2**24, t=data_array_sample[:, 0])
+
+# print(x)
+# print(y)
+
+plt.plot(data_array_sample[:, 0], data_array_sample[:, 1])
+plt.plot(x, y)
+plt.show()
+# print(data_array_sample)
