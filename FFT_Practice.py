a3f9d7f599436623b79436687f413a19ce282d66
Fleford <fleford@gmail.com>
2019-08-08 12:30:32 -0600

    found convolution parameters that work

--- FFT_Practice.py
+++ FFT_Practice.py
@@ -2,16 +2,19 @@ import numpy as np
 from scipy.fftpack import fft
 import matplotlib.pyplot as plt
 # Number of sample points
-N = 10000
+N = 100000
 # sample spacing
 T = 1.0 / 1000.0
-
 x = np.linspace(0.0, N*T, N)
-y = np.sin(50.0 * 2.0*np.pi*x) + 0.6*np.sin(100.0 * 2.0*np.pi*x)
-yf = fft(y)
-xf = np.linspace(0.0, 1.0/(2.0*T), N//2)    # Array of periods for the frequencies
-print(xf)
 
-plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+print(len(x))       # N = number of samples
+print((x.max() - x.min())/len(x))        # T = time interval
+
+y = np.sin(100.0 * 2.0*np.pi*x) + 0.6*np.sin(200.0 * 2.0*np.pi*x) + 2
+yf = fft(y)
+xf = np.linspace(0.0, 1.0/(2.0*T), num=N//2)    # Array of the frequencies
+# (0, 500)
+
+plt.plot(xf[0:(N//2)//1], 2.0/N * np.abs(yf[0:(N//2)//1]))
 plt.grid()
 plt.show()
--- WaveformAnalysis.py
+++ WaveformAnalysis.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
+from scipy.fftpack import fft
 
 # Import data
 data_array = np.loadtxt("CoolTerm Capture 2019-07-29 11-46-17.txt")
@@ -16,15 +17,61 @@ data_array = np.insert(data_array, 1, times_cumulative, axis=1)   # Insert time_
 data_array = np.delete(data_array, 0, axis=1)   # Remove interval time column
 
 # Pick out a sample of the data array
-window_start = 3
-window_size = 100
+print(len(data_array))
+window_start = 300
+window_size = 2**6
 data_array_sample = data_array[window_start:window_start+window_size, :]
-y, x = signal.resample(data_array_sample[:, 1], 2**24, t=data_array_sample[:, 0])
+# Remove offset
+print(data_array_sample[:, 1].mean())
+data_array_sample[:, 1] = data_array_sample[:, 1] - data_array_sample[:, 1].mean()
+N = np.int_(data_array_sample[:, 0].max() - data_array_sample[:, 0].min())
+print(N)
+s = 2**24
+print(N / s)
+print(144e6 /(N / s))
+y, x = signal.resample(data_array_sample[:, 1], s, t=data_array_sample[:, 0])
+print("Resample Done")
 
-# print(x)
-# print(y)
+# N = len(x)     # Number of samples
+# T = (x.max() - x.min()) / N   # Time interval between samples
+# print(N, T)
 
+# # Apply fft
+# yf = fft(y)
+# xf = np.linspace(0.0, 1.0/(2.0*T), N//2)    # Array of periods for the frequencies
+
+# Show plot for input waveforms
 plt.plot(data_array_sample[:, 0], data_array_sample[:, 1])
 plt.plot(x, y)
 plt.show()
-# print(data_array_sample)
+
+# # Show plot for FFT
+# plt.plot(xf[1:(N//2)//100000], 2.0/N * np.abs(yf[1:(N//2)//100000]))
+# print(np.abs(yf[1:(N//2)//100000]).argmax())
+# plt.grid()
+# plt.show()
+
+# correlate = np.correlate(y, y, "full")
+# plt.plot(correlate)
+# plt.show()
+
+
+# convolve = np.convolve(y, y, "same")
+# print(len(convolve))
+convolve = signal.fftconvolve(y, y, mode='full')
+print(len(convolve))
+plt.plot(convolve)
+plt.show()
+
+# convolve2 = np.convolve(convolve, convolve, "full")
+# plt.plot(convolve2)
+# plt.show()
+#
+# convolve3 = np.convolve(convolve2, convolve2, "full")
+# plt.plot(convolve3)
+# plt.show()
+#
+# convolve4 = np.convolve(convolve3, convolve3, "full")
+# plt.plot(convolve4)
+# plt.show()
+
